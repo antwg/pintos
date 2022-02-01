f13701a728142d8f6b502c2e82774eaf57a3d965
antwe841 <antwe841@su03-109.ad.liu.se>
2022-02-01 16:22:31 +0100

    write system call implemented

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -1,4 +1,4 @@
-#include "syscall.h"
+#include "userprog/syscall.h"
 #include <stdio.h>
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
@@ -16,9 +16,46 @@ static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
   printf ("system call!\n");
-  thread_exit ();
+
+  int syscall_nr = * (int*) f->esp;
+
+  printf("syscall number is %d\n", syscall_nr);
+
+  switch(syscall_nr){
+    case SYS_HALT:
+    {
+      halt_();
+      break;
+    }
+    case SYS_EXIT:
+    {
+      thread_exit();
+      break;
+    }
+    case SYS_CREATE:
+    {
+      //create_(syscall_nr);
+      break;
+    }
+    case SYS_WRITE:
+    {
+      write_call(f);
+      break;
+    }
+  }
 }
 
-void halt (void){
+void halt_(){
   power_off();
+}
+
+void write_call (struct intr_frame *f){
+  int fd = *(int*) (f-> esp + 4);
+  const void *buffer = *(void**) (f->esp + 8);
+  unsigned size = *(unsigned*) (f->esp + 12);
+  if (fd == 1){
+    putbuf(buffer, size);
+    f -> eax = size;
+  }
+  return;
 }
--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -3,6 +3,9 @@
 
 void syscall_init (void);
 
-void halt (void);
+//void halt_(void);
+
+//void write_call (struct intr_frame *f);
+//bool create_(int* syscall_nr);
 
 #endif /* userprog/syscall.h */
